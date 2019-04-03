64020d33e6aeb699344bbcd9e0bec7ff449eeb25
Epoy01 <48237449+Epoy01@users.noreply.github.com>
2019-04-03 22:58:23 +0800

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,5 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::resource('contacts','ContactController');
 
 Route::resource('cms','CustomerController');
+
+Route::resource('students','StudentController');
